818b4f607187e5947ffc35d2079f16f850109ae7
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-05-31 18:57:41 -0300

    finally transparency is working in neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,5 +8,23 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+
+		-- Ensure NeoTree uses the same background (chatGPT said this)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "neo-tree",
+			callback = function()
+				vim.opt_local.winhighlight = "Normal:Normal,NormalNC:Normal"
+			end,
+		})
+
+		-- General transparency settings for Neovim (chatGPT also said this)
+		vim.opt.termguicolors = true
+		vim.cmd([[
+        highlight Normal guibg=NONE ctermbg=NONE
+        highlight NonText guibg=NONE ctermbg=NONE
+        highlight NormalNC guibg=NONE ctermbg=NONE
+        highlight EndOfBuffer guibg=NONE ctermbg=NONE
+        highlight SignColumn guibg=NONE ctermbg=NONE
+      ]])
 	end,
 }
